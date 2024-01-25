87eb9ed09ad558f70fc59f7e6208d3253b457bbe
legodev <gonzalezleonardo283@gmail.com>
2024-01-25 19:49:08 -0300

    News components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
 import Header from './Header'
+import Footer from './components/Footer'
+import Intro1 from './components/Intro1'
+import Intro2 from './components/Intro2'
+import Intro3 from './components/Intro3'
+import Nav from './components/Nav'
+import Promo from './components/Promo'
+import Heading from './components/Heading'
 
 function App() {
   return (
     <>
       <Header />
+      <Nav/>
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
+      <Heading />
     </>
   )
 }
