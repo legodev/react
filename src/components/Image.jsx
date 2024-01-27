d89bf94c9437492debccf883b78b95e05c59c612
legodev <gonzalezleonardo283@gmail.com>
2024-01-26 23:56:19 -0300

    added styles to image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,13 @@
 import avatar from '../assets/image.jpg'
 
+const styles = {
+  border: '2px solid #121212',
+  borderRadius: '900px',
+  width: '250px',
+  height: '250px',
+}
+
 export default function Image() {
-  const logo = <img src={avatar} alt="" />
+  const logo = <img src={avatar} alt="" style={styles}/>
   return logo
 }
