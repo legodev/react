766d275a3a5ece83f6c5fddae026f2afed11eb07
legodev <gonzalezleonardo283@gmail.com>
2024-01-30 17:37:45 -0300

    New form

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,43 @@
 import { useState } from 'react'
 import './App.css'
-// import Header from './Header'
-// import Footer from './components/Footer'
-// import Intro1 from './components/Intro1'
-// import Intro2 from './components/Intro2'
-// import Intro3 from './components/Intro3'
-// import Nav from './components/Nav'
-// import Promo from './components/Promo'
-// import Heading from './components/Heading'
-// import Example from './components/Practice'
-// import PracticeTwo from './components/Practice2'
-// import Image from './components/Image'
-// import ModeToggler from './components/ModeToggler'
 
 function App() {
-  let randonNum = Math.floor(Math.random() * 10) + 1
-  // let numInput = prompt("Guess the number between 1 and 3")
-  // alert(`Computer number: ${randonNum}, Your guess: ${numInput}`)
+  const [form, setForm] = useState({
+    firstName: 'Leonardo',
+    lastName: 'González',
+    email: 'elmaslindo@gmail.com',
+  })
 
-  const [inputValue, setInputValue] = useState('')
-  const [message, setMessage] = useState('')
+  function handleFirstName(e) {
+    setForm({ ...form, firstName: e.target.value });
+  }  
 
-  const handleChange = (e) => {
-    setInputValue(e.target.value)
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (inputValue == randonNum) {
-      setMessage("Congratulations! You guessed the number")
-    } else {
-      setMessage("Almost... Try again!")
-    }
-  }
+  function handleLastName (e) {
+    setForm({ ...form, lastName: e.target.value });
+  } 
+
+  function handleEmail (e) {
+    setForm({ ...form, email: e.target.value });
+  } 
   return (
     <>
-      <div>
-        <h1>Guess the number between 1 and 10</h1>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input type="number" onChange={handleChange} />
-        <input type="submit" value="Guess!" />
+      <form action="">
+        <label htmlFor="first">
+          First Name:
+          <input type="text" id='first' onChange={handleFirstName} value={form.firstName} />
+        </label>
+        <label htmlFor="last">
+          Last Name:
+          <input type="text" id='last' onChange={handleLastName} value={form.firstName} />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input type="email" id='email' onChange={handleEmail} value={form.firstName} />
+        </label>
+        <p>{form.firstName}</p>
+        <p>{form.lastName}</p>
+        <p>{form.email}</p>
       </form>
-      <p>{message}</p>
-      {/* <ModeToggler /> */}
-      {/* <Image />
-      <Header name="Anna" color="blue"/>
-      <Nav/>
-      <PracticeTwo />
-      <Example />
-      <Heading />
-      <Promo />
-      <Intro1 />
-      <Intro2 />
-      <Intro3 />
-      <Footer /> */}
     </>
   )
 }
