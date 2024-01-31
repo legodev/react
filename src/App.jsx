c6e0b8b8e139e7278bc4a0dbd52e5ac91eacb39b
legodev <gonzalezleonardo283@gmail.com>
2024-01-31 18:12:47 -0300

    Practicing useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import { useState } from 'react'
+
+import { useRef } from "react"
 
 export default function Counter() {
-  const [number, setNumber] = useState(0);
-  console.log(number)
-
+  const [number, setNumber] = useState(0)
+  let ref = useRef(0)
+  console.log(ref.current)
   return (
     <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(number + 1);
-        setNumber(number + 8);
-        setNumber(number + 1);
-      }}>+3</button>
+      <h1>Number: {number}</h1>
+      <h1>Ref: {ref.current}</h1>
+      <button
+        onClick={() => {
+          setNumber(number + 1);
+          ref.current = ref.current + 1;
+        }}
+      >
+        +3
+      </button>
     </>
   )
 }
