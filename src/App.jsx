6a0064fee48b0b13aa01c0f19327fdbb5e3327fe
legodev <gonzalezleonardo283@gmail.com>
2024-01-31 17:38:06 -0300

    Problems useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
 import { useState } from 'react';
 
-export default function Stopwatch() {
-  const [startTime, setStartTime] = useState(null);
-  const [now, setNow] = useState(null);
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+  console.log(number)
 
-  function handleStart() {
-    setStartTime(Date.now());
-    setNow(Date.now());
-
-    setInterval(() => {
-      setNow(Date.now());
-    }, 10);
-  }
-
-  function handleEnd() {
-    setStartTime(null)
-    setNow(null)
-  }
-
-  let secondsPassed = 0;
-  if (startTime != null && now != null) {
-    secondsPassed = (now - startTime) / 1000;
-  }
-
-  function handleRestart() {
-    handleStart()
-  }
   return (
     <>
-      <h1>Tiempo transcurrido: {secondsPassed.toFixed(3)}</h1>
-      <button onClick={handleStart}>
-        Iniciar
-      </button>
-      <button onClick={handleEnd}>Parar</button>
-      <button onClick={handleRestart}>Restart</button>
+      <h1>{number}</h1>
+      <button onClick={() => {
+        setNumber(number + 1);
+        setNumber(number + 8);
+        setNumber(number + 1);
+      }}>+3</button>
     </>
-  );
+  )
 }
