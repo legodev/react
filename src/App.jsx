bc1324b76995ae12902317b01006c30b3f4294d9
legodev <gonzalezleonardo283@gmail.com>
2024-01-30 23:31:22 -0300

    Stopwatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 // import { useState } from 'react'
-import './App.css'
-import Form from './components/Form'
+import { useState } from 'react';
 
-function App() {
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
 
+  function handleStart() {
+    // Empieza a contar.
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    setInterval(() => {
+      // Actualiza el tiempo actual cada 10 milisegundos.
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function handleEnd() {
+    setStartTime(null)
+    setNow(null)
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  function handleRestart() {
+    setStartTime(0)
+    setNow(0)
+    handleStart()
+  }
   return (
     <>
-    <Form />
+      <h1>Tiempo transcurrido: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>
+        Iniciar
+      </button>
+      <button onClick={handleEnd}>Parar</button>
+      <button onClick={handleRestart}>Restart</button>
     </>
-  )
+  );
 }
-
-export default App
