4dad5395c12f8ba7bc4145d41f8a16afa9f98e32
legodev <gonzalezleonardo283@gmail.com>
2024-01-31 19:27:47 -0300

    useRef is not rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 import { useRef } from "react"
 
 export default function Counter() {
-  const [number, setNumber] = useState(0)
+  // const [number, setNumber] = useState(0)
   let ref = useRef(0)
-  console.log(ref.current)
+  // console.log(ref.current)
   return (
     <>
-      <h1>Number: {number}</h1>
-      <h1>Ref: {ref.current}</h1>
+      {/* <h1>Number: {number}</h1> */}
+      <h1>Ref: 0 + {ref.current}</h1>
       <button
         onClick={() => {
-          setNumber(number + 1);
+          // setNumber(number + 1);
           ref.current = ref.current + 1;
+          console.log(ref.current)
         }}
       >
         +3
