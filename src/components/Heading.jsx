4c6ec06fc2c82d9a0787c2e7667cc90c4127ca0b
legodev <gonzalezleonardo283@gmail.com>
2024-02-01 19:08:18 -0300

    Added Heading Component

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,5 +1,22 @@
-export default function Heading () {
-    return (
-        <h1>This is an h1 element</h1>
-    )
+import { useContext } from 'react'
+import { LevelContext } from '../hooks/levelContext'
+
+export default function Heading({ children }) {
+  const level = useContext(LevelContext)
+  switch (level) {
+    case 1:
+      return <h1>{children}</h1>
+    case 2:
+      return <h2>{children}</h2>
+    case 3:
+      return <h3>{children}</h3>
+    case 4:
+      return <h4>{children}</h4>
+    case 5:
+      return <h5>{children}</h5>
+    case 6:
+      return <h6>{children}</h6>
+    default:
+      throw Error('Unknown level' + { level })
+  }
 }
