cb76111660f2644e98920b5147da42cf2d9ab27b
legodev <gonzalezleonardo283@gmail.com>
2024-02-01 19:49:25 -0300

    Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import Section from "./components/Section"
-import Heading from "./components/Heading"
+import Section from './components/Section'
+import Heading from './components/Heading'
 
 export default function App() {
-
   return (
     <>
       <Section level={1}>
-        <Heading>
-          Hello World
-        </Heading>
+        <Heading>Hello World</Heading>
         <Section level={6}>
-          <Heading>
-            Bye Bye World
-          </Heading>
+          <Heading>Bye Bye World</Heading>
+          <section>
+            <Heading>Bye Bye World 2 h6</Heading>
+          </section>
           <Section level={2}>
             <Heading>Another World</Heading>
+            <div>
+              <Heading>Another World h2</Heading>
+            </div>
           </Section>
         </Section>
       </Section>
