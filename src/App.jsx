5ebd05d000341e2e4c80ed3b0d58ab66d1cb75b8
legodev <gonzalezleonardo283@gmail.com>
2024-01-31 23:57:35 -0300

    Exercice random number

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-// import { useState } from 'react'
-import { useRef } from 'react'
+import { useState } from 'react'
 
 export default function TextInputWithFocusButton() {
-  const inputEl = useRef(null)
-  const onButtonClick = () => {
-    // `current` points to the mounted text input element
-    inputEl.current.focus()
+  const [message,setMessage] = useState(0)
+  function handleClick () {
+    setMessage(Math.round(Math.random() * 10))
   }
-
-  const fruits = ['Pear', 'Banana', 'Apple', 'Orange', 'Pineapple']
-  const [f1,f2,f3] = fruits
-  console.log(f1,f2,f3);
-
   return (
     <>
-      <input ref={inputEl} type="text" />
-      <button onClick={onButtonClick}>Focus the input</button>
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Random</button>
     </>
   )
 }
