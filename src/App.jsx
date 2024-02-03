61256313c501c56d39140d756903385a0cbfd4aa
legodev <gonzalezleonardo283@gmail.com>
2024-02-02 23:42:52 -0300

    Added new templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import Title from './components/Title'
 import About from './components/About'
+import './App.css'
 
 export default function App() {
   return (
     <div>
+      <nav className='navbar'>
+        <Link to="/">Home</Link>
+        <Link to="/about-me">About Me</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Title />} />
         <Route path="/about-me" element={<About />} />
