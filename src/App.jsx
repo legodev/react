8df243a33e3ec62e20f64853360116a352838941
legodev <gonzalezleonardo283@gmail.com>
2024-02-05 01:46:15 -0300

    Added new audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import ReactPlayer from 'react-player/youtube'
 
 function App() {
+  const audio = new Audio(
+"https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-100509/zapsplat_industrial_tool_axe_chop_wood_forest_echo_single_001_102072.mp3"  )
+  function toggleAudio() {
+    if (audio.paused) {
+      audio.play()
+    } else {
+      audio.pause()
+    }
+  }
+
   return (
     <div className="app">
       <h1>I ❤️💝 you!</h1>
       <MyVideo />
+      <button onClick={toggleAudio}>Audio</button>
     </div>
   )
 }
@@ -14,7 +25,12 @@ export default App
 function MyVideo() {
   return (
     <div>
-      <ReactPlayer volume={0.01} playing={false} controls url="https://www.youtube.com/watch?v=G--7wpx3ndc&list=RDG--7wpx3ndc&start_radio=1&ab_channel=duetzezowaty"></ReactPlayer>
+      <ReactPlayer
+        volume={0.4}
+        playing={false}
+        controls
+        url="https://www.youtube.com/watch?v=G--7wpx3ndc&list=RDG--7wpx3ndc&start_radio=1&ab_channel=duetzezowaty"
+      ></ReactPlayer>
     </div>
   )
 }
